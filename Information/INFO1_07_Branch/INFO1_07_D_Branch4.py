7fadeac1d055597c095bb86d0aeedb14514d8d72
WaiTeng298 <167703518+CWTeng1005@users.noreply.github.com>
2025-06-24 21:20:00 +0900

    updated

---
+++ Information/INFO1_07_Branch/INFO1_07_D_Branch4.py
@@ -0,0 +1,11 @@
+x = int(input())
+if 80 <= x <= 100:
+    print("A")
+elif 65 <= x < 80:
+    print("B")
+elif 50 <= x < 65:
+    print("C")
+elif 35 <= x < 50:
+    print("D")
+elif 0 <= x < 35:
+    print("F")
